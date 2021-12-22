5885d95976e26403ec4b3a8eb748726ae874823e
Nedwards0 <nedwards0001@gmai.com>
2021-12-22 00:00:20 -0800

    Added test data

--- datalogger.py
+++ datalogger.py
@@ -7,23 +7,32 @@ import pandas
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image as im
-
+import pandas as pd
 x=[]
+y=[]
+time.sleep(10)
 try:
     count=0
     while True:
         count+=1
         now = datetime.now()
         screenshot = pyautogui.screenshot()
+        #print(screenshot)
         current_time = now.strftime("%H:%M:%S")
         print(current_time)
         image = (np.array(screenshot))
-        x.append(image)
-        print(type(image))
+        print(image)
+        if(count==1):
+            df = pd.DataFrame({'x':[image], 'y':1}, columns=['x','y'])
+        else:
+            df2 = pd.DataFrame({'x':[image], 'y':1}, columns=['x','y'])
+            frames=[df,df2]
+            df = pd.concat(frames)
         data = im.fromarray(image)
         data.save(str("ImagesData/")+str(count)+'.png')
+
         time.sleep(5)
 except KeyboardInterrupt:
-    print(x)
+    df.to_csv('data.csv')
 
 
